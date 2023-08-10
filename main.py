fc2671a59ba2d8bb8b5ab1806e38f87dddb52bf9
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2023-08-09 20:53:35 -0700

    able to communicate with api now

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import os
+from notion_client import Client
 import config
 
-DATABASE_ID = "Spots-66f3a67b298140eb9e81e696fac300d5?pvs=4"
+
 import requests
 from datetime import datetime, timezone
 
-
-headers = {
-    "Authorization": "Bearer " + config.API_TOKEN,
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28"
-}
 
 
 def get_pages():
     url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
     payload = {"page_size": 100}
     response = requests.post(url,json=payload,headers=headers)
-
     data = response.json
+    pass
+
+
+def get_token():
+    txt_object = open("api.txt", "r")
+    api_token = txt_object.read()
+    txt_object.close()
+    return api_token
+
+
+def main():
+    api_key = get_token()
+    notion = Client(auth=api_key)
+    print (notion.users.list())
+
+main()
